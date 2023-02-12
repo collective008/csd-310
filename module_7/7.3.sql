f9f63c1b9baf1f62adac52c0c16974bdccc3ed50
collective008 <104705369+collective008@users.noreply.github.com>
2023-02-12 12:37:29 -0500

    Add files via upload

---
+++ module_7/7.3.sql
@@ -0,0 +1,44 @@
+DROP USER IF EXISTS 'pysports_user'@'localhost';
+
+CREATE USER 'pysports_user'@'localhost' IDENTIFIED WITH mysql_native_password BY 'MySQL8IsGreat!';
+
+GRANT ALL PRIVILEGES ON pysports.* TO'pysports_user'@'localhost';
+
+DROP TABLE IF EXISTS player;
+DROP TABLE IF EXISTS team;
+
+CREATE TABLE team (
+    team_id     INT             NOT NULL        AUTO_INCREMENT,
+    team_name   VARCHAR(75)     NOT NULL,
+    mascot      VARCHAR(75)     NOT NULL,
+    PRIMARY KEY(team_id)
+); 
+
+CREATE TABLE player (
+    player_id   INT             NOT NULL        AUTO_INCREMENT,
+    first_name  VARCHAR(75)     NOT NULL,
+    last_name   VARCHAR(75)     NOT NULL,
+    team_id     INT             NOT NULL,
+    PRIMARY KEY(player_id),
+    CONSTRAINT fk_team 
+    FOREIGN KEY(team_id)
+        REFERENCES team(team_id)
+);
+
+INSERT INTO team(team_name, mascot)
+    VALUES('Milwaukee_Bucks', 'Deer');
+
+INSERT INTO team(team_name, mascot)
+    VALUES('Chicago_Bulls', 'Bull');
+
+INSERT INTO player(first_name, last_name, team_id) 
+    VALUES('Michael', 'Fasbender', (SELECT team_id FROM team WHERE team_name = 'Milwaukee_Bucks'));
+
+INSERT INTO player(first_name, last_name, team_id)
+    VALUES('Brad', 'Pitt', (SELECT team_id FROM team WHERE team_name = 'Milwaukee_Bucks'));
+
+INSERT INTO player(first_name, last_name, team_id) 
+    VALUES('Chris', 'Cornell', (SELECT team_id FROM team WHERE team_name = 'Chicago_Bulls'));
+
+INSERT INTO player(first_name, last_name, team_id)
+    VALUES('Layne', 'Stayley', (SELECT team_id FROM team WHERE team_name = 'Chicago_Bulls'));
