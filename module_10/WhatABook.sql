c08fa00af88a7ac3602455f3e2a1d97f2e405a89
collective008 <104705369+collective008@users.noreply.github.com>
2023-02-18 23:00:49 -0500

    WhatABook.sql
    WhatABook.sql

--- module_10/WhatABook.sql
+++ module_10/WhatABook.sql
@@ -16,21 +16,21 @@ DROP TABLE IF EXISTS wishlist;
 DROP TABLE IF EXISTS user;
 
 CREATE TABLE user (
-    user_id   INT               NOT NULL        AUTO_INCREMENT,
+    user_id   INT               NOT NULL       AUTO_INCREMENT,
     first_name  VARCHAR(75)     NOT NULL,
     last_name   VARCHAR(75)     NOT NULL,
     PRIMARY KEY(user_id)
 );
 CREATE TABLE book (
-    book_id       INT           NOT NULL        AUTO_INCREMENT,
-    book_names    VARCHAR(200)  NOT NULL,
-    details       VARCHAR(500),
-    author        VARCHAR(200)  NOT NULL,
+    book_id      INT           NOT NULL        AUTO_INCREMENT,
+    book_name    VARCHAR(200)  NOT NULL,
+    details      VARCHAR(500),
+    author       VARCHAR(200)  NOT NULL,
     PRIMARY KEY(book_id)
 );
 CREATE TABLE store (
-    store_id   INT              NOT NULL,
-    locale    VARCHAR(500)      NOT NULL,
+    store_id   INT             NOT NULL        AUTO_INCREMENT,
+    locale     VARCHAR(500)    NOT NULL,
     PRIMARY KEY(store_id)
 );
 CREATE TABLE wishlist (
@@ -46,17 +46,15 @@ CREATE TABLE wishlist (
         REFERENCES user(user_id)
 );
 
---store
 INSERT INTO store(locale)
     VALUES('311 East Main Street, Waukesha, Wisconsin, 53189');
 
---books
 INSERT INTO book(book_name, author, details)
     VALUES('YoYo 101', 'M. Duncan', 'Lets Play YoYos!');
 INSERT INTO book(book_name, author, details)
     VALUES('Build a Plastic World', 'Ole Kirk Kristiansen', 'How to use Legos to build great things');
 INSERT INTO book(book_name, author, details)
-    VALUES('GI Joe 1983-1990', 'Mark Belomo', "Ultimate Guide to Vintage GI Joe");
+    VALUES('GI Joe 1983-1990', 'Mark Belomo', 'Ultimate Guide to Vintage GI Joe');
 INSERT INTO book(book_name, author)
     VALUES('Python 101', 'Joe Smith');
 INSERT INTO book(book_name, author)
@@ -69,14 +67,14 @@ INSERT INTO book(book_name, author)
     VALUES('Lizads', 'T. Rex');
 INSERT INTO book(book_name, author)
     VALUES('Bugs', 'A. A. Ant');
---name
+
 INSERT INTO user(first_name, last_name) 
     VALUES('John', 'Doe');
 INSERT INTO user(first_name, last_name)
     VALUES('Jane', 'Doe');
 INSERT INTO user(first_name, last_name)
     VALUES('Abraham', 'Lincon');
---wishlist
+
 INSERT INTO wishlist(user_id, book_id) 
     VALUES (
         (SELECT user_id FROM user WHERE first_name = 'John'), 
@@ -85,7 +83,7 @@ INSERT INTO wishlist(user_id, book_id)
 INSERT INTO wishlist(user_id, book_id)
     VALUES (
         (SELECT user_id FROM user WHERE first_name = 'Jane'),
-        (SELECT book_id FROM book WHERE book_name = 'Build a Plastic World')
+        (SELECT book_id FROM book WHERE book_name = 'build a Plastic World')
     );
 INSERT INTO wishlist(user_id, book_id)
     VALUES (
